5dd062a20de3f0b22993fe560e441589682da9cd
gshunc <111393220+gshunc@users.noreply.github.com>
2022-11-06 05:50:46 +0000

    Deploying to gh-pages from @ gshunc/stupidhackathon@810e214fc6ead3c3e2748d9b8dedee857bf4a281 🚀

--- contact.a18d228e.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".burger"),t=document.querySelector("#"+e.dataset.target);e.addEventListener("click",(function(){e.classList.toggle("is-active"),t.classList.toggle("is-active")}))}();
-//# sourceMappingURL=contact.a18d228e.js.map
