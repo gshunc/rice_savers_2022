66b73d8967a493ee7eda5fab355b41b70bb6d048
robert :) <robertlewison@gmail.com>
2022-11-05 21:25:58 -0400

    changes

--- app.py
+++ app.py
@@ -6,14 +6,15 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
 @app.route('/', methods=['POST'])
 def predict():
     imagefile = request.files['imagefile']
     image_path = "./images/" + imagefile.filename
     imagefile.save(image_path)
 
-
     return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(port=3000, debug=True)
